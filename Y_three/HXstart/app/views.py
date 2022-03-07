2bc5f6673659bcb2b5178264f45b1efc707302e1
HXUbuntu <z3407279207@gmail.com>
2022-03-06 22:35:25 -0800

    mako配置使用

--- Y_three/HXstart/app/views.py
+++ Y_three/HXstart/app/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from .base_render import render_to_response
+from django.views.generic import View
+
 
 # Create your views here.
+
+class Index(View):
+    def get(self,request):
+        data = {'name':'zhengxin','age':20}
+        return render_to_response(request,'index.html',data=data)
+
