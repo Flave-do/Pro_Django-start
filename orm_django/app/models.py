d1a98aefc9b9fd0290b4c672a14e2b2c877d38aa
HXUbuntu <z3407279207@gmail.com>
2022-03-07 18:29:01 -0800

    新建数据库迁移，有点小问题

--- orm_django/app/models.py
+++ orm_django/app/models.py
@@ -2,6 +2,16 @@ from django.db import models
 
 # Create your models here.
 # 数据模型----基于类的
-class Test(models.Model):
-    name = models.CharField(max_length=20)
-    age = models.IntegerField(default=0)
+class User(models.Model):
+    id = models.IntegerField(primary_key=True)
+    # uuique 唯一索引 该值不允许重复 该值不允许为空
+    username = models.CharField(unique=True,max_length=20,blank=False)
+    age = models.SmallIntegerField(default=0)
+    phone = models.SmallIntegerField(db_column=True,blank=True,default=0)
+    email = models.EmailField(blank=True,default='')
+    info = models.TextField()
+
+    # 创建时添加时间
+    create_time = models.DateTimeField(auto_now_add=True)
+    # 更新时变更时间
+    update_time = models.DateTimeField(auto_now=True)
