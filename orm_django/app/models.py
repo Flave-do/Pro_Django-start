ab865eb719a9b568602a5f0795146e9175158cda
HXUbuntu <z3407279207@gmail.com>
2022-03-07 18:50:09 -0800

    小问题解决，修正单词错误删除数据库并重新迁移

--- orm_django/app/models.py
+++ orm_django/app/models.py
@@ -4,10 +4,10 @@ from django.db import models
 # 数据模型----基于类的
 class User(models.Model):
     id = models.IntegerField(primary_key=True)
-    # uuique 唯一索引 该值不允许重复 该值不允许为空
+    # uique 唯一索引 该值不允许重复 该值不允许为空
     username = models.CharField(unique=True,max_length=20,blank=False)
     age = models.SmallIntegerField(default=0)
-    phone = models.SmallIntegerField(db_column=True,blank=True,default=0)
+    phone = models.SmallIntegerField(db_index=True,blank=True,default=0)
     email = models.EmailField(blank=True,default='')
     info = models.TextField()
 
