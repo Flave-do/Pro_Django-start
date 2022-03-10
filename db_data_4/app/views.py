11a79aa94eebe4e54a797ed039879ed99999dbf6
HXUbuntu <z3407279207@gmail.com>
2022-03-09 23:31:07 -0800

    导入数据方便查询第一种

--- db_data_4/app/views.py
+++ db_data_4/app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from .models import User,Userprofile,Group,Diary
+from django.views.generic import View
 # Create your views here.
+
+class User_Data(View):
+    def get(self,request):
+        user = User()
+        user.username = '正行'
+        user.age = 18
+        user.email = 'zhengxing@qq.com'
+        user.phone = '15111122211'
+        user.info = '阿坝阿坝'
+        user.save()
