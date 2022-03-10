84dee5404882d6d6ecb19a5b8035d28d4c0f40f7
HXUbuntu <z3407279207@gmail.com>
2022-03-09 17:37:54 -0800

    os.path.join(BASE_DIR, 'templates')

--- db_data_3/app/views.py
+++ db_data_3/app/views.py
@@ -7,13 +7,16 @@ from django.views.generic import View
 class UserInfo(View):
     def get(self,request):
         # 创建数据
-        User.objects.create(username='正行',age=20)
+        # User.objects.create(username='正行',age=20)
 
         # user = User(username='马克',age=18)
         # user.save()
 
-        user = User()
-        user.username = '山河'
-        user.age = 21
-        user.save()
-        return render(request,'UserInfo.html')
+        # user = User()
+        # user.username = '山河'
+        # user.age = 21
+        # user.save()
+
+        # 查询方法
+        user = User.objects.get(id=1)
+        return render(request,'UserInfo.html',{'name':user.username})
