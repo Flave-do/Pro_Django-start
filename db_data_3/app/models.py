e652a7f0cc02d39ba3037a350ae5711349caea50
HXUbuntu <z3407279207@gmail.com>
2022-03-09 17:48:05 -0800

    查询全部数据并定义一函数方便显示

--- db_data_3/app/models.py
+++ db_data_3/app/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class User(models.Model):
     username = models.CharField(max_length=20)
     age = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.username
