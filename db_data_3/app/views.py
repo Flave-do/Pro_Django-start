aadbf30b73a8a6d2c9b81ce6e02e0bebacc2574e
HXUbuntu <z3407279207@gmail.com>
2022-03-09 17:25:40 -0800

    增

--- db_data_3/app/views.py
+++ db_data_3/app/views.py
@@ -8,4 +8,6 @@ class UserInfo(View):
     def get(self,request):
         # 创建数据
         User.objects.create(username='正行',age=20)
+        user = User(username='马克',age=18)
+        user.save()
         return render(request,'UserInfo.html')
