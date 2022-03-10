187c8c812bb509ad8dc9a9c38a820153ab7543f2
HXUbuntu <z3407279207@gmail.com>
2022-03-09 23:57:46 -0800

    修改一个小写引发的错误

--- db_data_4/app/views.py
+++ db_data_4/app/views.py
@@ -7,11 +7,11 @@ from django.views.generic import View
 class User_Data(View):
     def get(self,request):
         user = User()
-        user.username = '正行'
+        user.username = '正新'
         user.age = 18
         user.email = 'zhengxing@qq.com'
         user.phone = '1511112221'
         user.info = '阿坝阿坝'
         user.save()
-        user.objects.create(username='马克',age=20,email='make@qq.com',phone='1301231144',info='嘻嘻嘻吧')
+        User.objects.create(username='马克',age=20,email='make@qq.com',phone='1301231144',info='嘻嘻嘻吧')
         return HttpResponse('successful')
