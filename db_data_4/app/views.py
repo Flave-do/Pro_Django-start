7921f582a89f33397e9c6f563cec97786f706cdd
HXUbuntu <z3407279207@gmail.com>
2022-03-10 00:06:01 -0800

    接下来都是查询了

--- db_data_4/app/views.py
+++ db_data_4/app/views.py
@@ -15,19 +15,24 @@ class User_Data(View):
         # user.save()
         # User.objects.create(username='马克',age=20,email='make@qq.com',phone='1301231144',info='嘻嘻嘻吧')
 
-        user = User.objects.get(id=1)
-        user_profile = Userprofile()
-        user_profile.user = user
-        user_profile.birthday = '1995-11-25'
-        user_profile.save()
+        # user = User.objects.get(id=1)
+        # user_profile = Userprofile()
+        # user_profile.user = user
+        # user_profile.birthday = '1995-11-25'
+        # user_profile.save()
+        #
+        # diary = Diary()
+        # diary.user = user
+        # diary.content = '正行老斯，我们一起学怕从'
+        # diary.save()
+        #
+        # group = Group()
+        # group.name = '开发'
+        # group.save()
+        # group.user.add(user)
 
-        diary = Diary()
-        diary.user = user
-        diary.content = '正行老斯，我们一起学怕从'
-        diary.save()
-
-        group = Group()
-        group.name = '开发'
-        group.save()
-        group.user.add(user)
+        # 查询
+        zhengxing = User.objects.get(pk=1)
+        data = zhengxing.diary.values('content')
+        print(data)
         return HttpResponse('successful')
