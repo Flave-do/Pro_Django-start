f490a96087d63f2ce0066db737cb24a8f6002311
HXUbuntu <z3407279207@gmail.com>
2022-03-09 17:16:44 -0800

    模板，视图函数配置

--- db_data_3/app/views.py
+++ db_data_3/app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import User
+from django.views.generic import View
+
+class UserInfo(View):
+    def get(self,request):
+        # 创建数据
+        User.objects.create(username='正行',age=20)
+        return render(request,'UserInfo.html')
