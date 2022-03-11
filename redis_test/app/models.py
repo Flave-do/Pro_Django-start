4c113987b5a93f1656fbeb535e5c115f2c79f7f6
HXUbuntu <z3407279207@gmail.com>
2022-03-10 19:11:45 -0800

    模型类配置

--- redis_test/app/models.py
+++ redis_test/app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    username = models.CharField(unique=True,max_length=20,blank=True)
+    age = models.SmallIntegerField(default=0)
+    phone = models.CharField(max_length=11)
+    email = models.EmailField(blank=True,default='')
+    info = models.TextField()
+    create_time = models.DateTimeField(auto_now_add=True)
+    update_time = models.DateTimeField(auto_now=True)
+
+
+    # 联合索引
+    class Meta:
+        index_together = ['username','phone']
+        
+    def __str__(self):
+        return self.username
