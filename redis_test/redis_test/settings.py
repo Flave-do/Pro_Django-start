d0657d377840e9a98fd34ef93d48d3587429e1bd
HXUbuntu <z3407279207@gmail.com>
2022-03-10 19:05:30 -0800

    基础配置

--- redis_test/redis_test/settings.py
+++ redis_test/redis_test/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9i#qrg6qza_lapr@@jn236%pkd-+hg#6s9akl6b@f)_i7)x!7k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'redis_test.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'user_redis_6',
+        'USER':'root',
+        'PASSWORD':'mysql',
+        'HOST':'',
+        'PORT':'3306',
     }
 }
 
