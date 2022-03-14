abc1094936b4447070bcd1c04e3b4379e02a66b6
HXUbuntu <z3407279207@gmail.com>
2022-03-14 00:17:25 -0700

    最简单的表单文件

---
+++ django_form_test_2/app/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from django.forms import fields
+
+class Auth(forms.Form):
+    username = fields.CharField(
+        # 限制用户输入的最大长度
+        max_length=10,
+        # 显示用户输入的最小长度
+        min_length=4,
+        label='用户名',
+    )
+    
+    password = fields.CharField(
+        widget=forms.PasswordInput(),
+        label='密码',
+        min_length=10,
+    )
