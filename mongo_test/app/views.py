97d56183212406024ff228e94eb188f90e32977d
HXUbuntu <z3407279207@gmail.com>
2022-03-13 19:36:31 -0700

    视图函数

--- mongo_test/app/views.py
+++ mongo_test/app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .mongo_engine import Users,Paper
+from django.http import HttpResponse
+from django.views.generic import View
 # Create your views here.
+
+class Mongo_User(View):
+    def get(self,request):
+        user = Users.objects.create(name='shanhe',age=20)
+        return HttpResponse('my name is {},age is {}'.format(user.name,user.age))
+    
