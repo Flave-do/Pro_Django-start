97aeb53d42c50640b1f945db239af9af912a6c5e
HXUbuntu <z3407279207@gmail.com>
2022-03-14 20:00:44 -0700

    用户-视图架子

--- user_test/app/views.py
+++ user_test/app/views.py
@@ -1,3 +1,38 @@
 from django.shortcuts import render
-
+from django.views.generic import View
 # Create your views here.
+
+# 注册
+class Register(View):
+    def get(self,request):
+        pass
+
+    def post(self,request):
+        pass
+
+
+# 登录
+class Login(View):
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
+
+
+# 网站首页
+class Index(View):
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
+
+
+# 注销
+class LogoutUser(View):
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
