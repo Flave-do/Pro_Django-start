b5fc469269056a256a11638a6ecd142a10536202
HXUbuntu <z3407279207@gmail.com>
2022-03-15 00:15:48 -0700

    用户-登录

--- user_test/app/views.py
+++ user_test/app/views.py
@@ -27,7 +27,7 @@ class Register(View):
         # 判断当前注册帐号是否已注册，如果已经被注册则提示已注册
         exists = User.objects.filter(username=username).exists()
         if exists:
-            return HttpResponse('该帐号已被注册')
+            return HttpResponse('该帐号已被注册 请重新注册')
         User.objects.create_user(username=username,password=password)
         return redirect(reverse('login'))
 
@@ -38,7 +38,19 @@ class Login(View):
         return render(request,'login.html')
 
     def post(self, request):
-        pass
+        username = request.POST.get('username','')
+        password = request.POST.get('password','')
+
+        # 判断当前用户是否存在，如果不存在则让用户重新注册
+        exists = User.objects.filter(username=username).exists()
+        if not exists:
+            return HttpResponse('该帐号不存在 请注册')
+        user = authenticate(username=username,password=password)
+        if user:
+            login(request,user)
+            return redirect(reverse('index'))
+        else:
+            return HttpResponse('密码错误')
 
 
 # 网站首页
