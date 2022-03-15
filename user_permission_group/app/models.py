b7b8cbd6e73e1fd593db38ed0587a152908f5cae
HXUbuntu <z3407279207@gmail.com>
2022-03-15 02:32:52 -0700

    用户组-模型类设置

--- user_permission_group/app/models.py
+++ user_permission_group/app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+class Apage(models.Model):
+    title = models.CharField(max_length=10)
+
+    def __str__(self):
+        return 'title:{}'.format(self.title)
+
+    class Meta:
+        permissions = [
+            ('look_a_page','can get this a page message')
+        ]
+
+
+class Bpage(models.Model):
+    title = models.CharField(max_length=10)
+
+    def __str__(self):
+        return 'title:{}'.format(self.title)
+
+    class Meta:
+        permissions = [
+            ('look_b_page','can get this a page message')
+        ]
